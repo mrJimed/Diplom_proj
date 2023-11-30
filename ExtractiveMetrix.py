639927538b25fb9d30fe56397e3dfd1cb713a12c
Danil <danil0316@mail.ru>
2023-11-30 23:59:56 +0500

    fix 'ExtractiveMetrix.py'

--- ExtractiveMetrix.py
+++ ExtractiveMetrix.py
@@ -13,12 +13,12 @@ def create_table(n_rows=15) -> PrettyTable:
     rouges = defaultdict(list)
     for idx, (text, orig_summary) in enumerate(zip(df_test['text'][:n_rows], df_test['summary'][:n_rows])):
         summarize = TfidfSummarizer.summarize_text(text)
-        scores = rouge.get_scores(summarize, orig_summary)
+        scores = rouge.get_scores(summarize, orig_summary)[0]
         for metric, value in scores.items():
             rouges[metric].append(value['f'])
     table = PrettyTable()
     table.field_names = ['', 'TF-IDF']
     for metric, values in rouges.items():
         avg_score = sum(values) / len(values)
-        table.add_row([[metric, avg_score]])
+        table.add_row([metric, avg_score])
     return table
