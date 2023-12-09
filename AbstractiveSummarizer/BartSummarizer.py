de7e98be5edb5d14d511edbc96210423a76879bf
Danil <danil0316@mail.ru>
2023-12-09 23:54:57 +0500

    update 'BartSummarizer.py'

--- AbstractiveSummarizer/BartSummarizer.py
+++ AbstractiveSummarizer/BartSummarizer.py
@@ -1,25 +1,27 @@
 from transformers import MBartTokenizer, MBartForConditionalGeneration
 import textwrap
+import torch
 
 
 class BartSummarizer:
     def __init__(self):
+        self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self._model_name = "IlyaGusev/mbart_ru_sum_gazeta"
         self._tokenizer = MBartTokenizer.from_pretrained(self._model_name)
-        self._model = MBartForConditionalGeneration.from_pretrained(self._model_name)
+        self._model = MBartForConditionalGeneration.from_pretrained(self._model_name).to(self._device)
 
     def _summarize(self, text, max_length, min_length):
-        tokenized_text = self._tokenizer.encode(text, return_tensors="pt")
+        tokenized_text = self._tokenizer.encode(text, return_tensors="pt").to(self._device)
         max_tokens = int(max_length * tokenized_text.size(1))
         min_tokens = int(min_length * tokenized_text.size(1))
 
         output_ids = self._model.generate(
             max_length=max_tokens,
             input_ids=tokenized_text,
-            no_repeat_ngram_size=3,
-            num_beams=8,
+            no_repeat_ngram_size=4,
+            num_beams=12,
             early_stopping=True,
-            length_penalty=0.9,
+            length_penalty=0.5,
             min_length=min_tokens
         )[0]
 
