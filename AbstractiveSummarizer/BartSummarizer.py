231448f7c361fa3fda125a5ba5229dee00602c25
Danil <danil0316@mail.ru>
2023-12-09 23:57:49 +0500

    update 'BartSummarizer.py'

--- AbstractiveSummarizer/BartSummarizer.py
+++ AbstractiveSummarizer/BartSummarizer.py
@@ -10,7 +10,7 @@ class BartSummarizer:
         self._tokenizer = MBartTokenizer.from_pretrained(self._model_name)
         self._model = MBartForConditionalGeneration.from_pretrained(self._model_name).to(self._device)
 
-    def _summarize(self, text, max_length, min_length):
+    def _summarize(self, text, max_length, min_length) -> str:
         tokenized_text = self._tokenizer.encode(text, return_tensors="pt").to(self._device)
         max_tokens = int(max_length * tokenized_text.size(1))
         min_tokens = int(min_length * tokenized_text.size(1))
@@ -28,7 +28,7 @@ class BartSummarizer:
         summary = self._tokenizer.decode(output_ids, skip_special_tokens=True)
         return summary
 
-    def summarize_text(self, text, chunk_size=500, max_length=0.7, min_length=0.4):
+    def summarize_text(self, text, chunk_size=500, max_length=0.7, min_length=0.4) -> str:
         summaries = []
         for chunk in textwrap.wrap(text, chunk_size):
             summary = self._summarize(chunk, max_length, min_length)
