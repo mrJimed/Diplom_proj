e69fe53479297080abcaf79e91abd1fdf0d69441
Danil <danil0316@mail.ru>
2023-12-10 18:00:30 +0500

    add _get_chunks

--- summarization/abstractive/BartSummarizer.py
+++ summarization/abstractive/BartSummarizer.py
@@ -1,6 +1,6 @@
-from transformers import MBartTokenizer, MBartForConditionalGeneration
-import textwrap
 import torch
+from nltk import sent_tokenize
+from transformers import MBartTokenizer, MBartForConditionalGeneration
 
 
 class BartSummarizer:
@@ -28,9 +28,23 @@ class BartSummarizer:
         summary = self._tokenizer.decode(output_ids, skip_special_tokens=True)
         return summary
 
+    def _get_chunks(self, text, max_length) -> list:
+        sentences = sent_tokenize(text)
+        chunks = []
+        current_sentence = ''
+        for sentence in sentences:
+            if len(current_sentence) + len(sentence) <= max_length:
+                current_sentence += f' {sentence}' if current_sentence else sentence
+            else:
+                chunks.append(current_sentence)
+                current_sentence = sentence
+        if current_sentence:
+            chunks.append(current_sentence)
+        return chunks
+
     def summarize_text(self, text, chunk_size=500, max_length=0.7, min_length=0.4) -> str:
         summaries = []
-        for chunk in textwrap.wrap(text, chunk_size):
+        for chunk in self._get_chunks(text, chunk_size):
             summary = self._summarize(chunk, max_length, min_length)
             summaries.append(summary)
         final_summary = ' '.join(summaries)
