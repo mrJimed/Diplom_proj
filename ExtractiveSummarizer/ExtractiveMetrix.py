a8425d8f51021659ffaf4faf1e2640bfdb126d9d
Danil <danil0316@mail.ru>
2023-12-10 12:02:32 +0500

    update 'ExtractiveMetrix.py'

--- ExtractiveSummarizer/ExtractiveMetrix.py
+++ ExtractiveSummarizer/ExtractiveMetrix.py
@@ -1,43 +1,28 @@
-from rouge import Rouge
+from collections import defaultdict
+
 import pandas as pd
 from datasets import load_dataset
-from ExtractiveSummarizer.TfidfSummarizer import TfidfSummarizer
-from ExtractiveSummarizer.LsaSummarizer import LsaSummarizer
-from collections import defaultdict
-from prettytable import PrettyTable
+from nltk import word_tokenize
+from nltk.translate import meteor_score
+from rouge import Rouge
 
 
-def tfidf_metrix(n_rows=15, average=False) -> PrettyTable:
+def extractive_metrix(summarize_method, sentences_count, n_rows=15):
     dataset = load_dataset('IlyaGusev/gazeta', revision="v2.0")
     df_test = pd.DataFrame(dataset['test'], columns=['text', 'summary'])
     rouge = Rouge()
     rouges = defaultdict(list)
+    meteors = []
     for idx, (text, orig_summary) in enumerate(zip(df_test['text'][:n_rows], df_test['summary'][:n_rows])):
-        summarize = TfidfSummarizer.summarize_text_average(text) if average else TfidfSummarizer.summarize_text(text)
-        scores = rouge.get_scores(summarize, orig_summary)[0]
+        print(f'Начал текст № {idx}')
+        summary = summarize_method(text, sentences_count=sentences_count)
+        tokenize_summary = word_tokenize(summary)
+        tokenize_orig_summary = word_tokenize(orig_summary)
+        meteors.append(meteor_score.meteor_score([tokenize_orig_summary], tokenize_summary))
+        scores = rouge.get_scores(summary, orig_summary)[0]
         for metric, value in scores.items():
             rouges[metric].append(value['f'])
-    table = PrettyTable()
-    table.field_names = ['', 'TF-IDF']
+        print(f'Закончил текст № {idx}\n')
+    print(f'METEOR: {sum(meteors) / len(meteors)}')
     for metric, values in rouges.items():
-        avg_score = sum(values) / len(values)
-        table.add_row([metric, avg_score])
-    return table
-
-
-def lsa_metrix(n_rows=15, average=False) -> PrettyTable:
-    dataset = load_dataset('IlyaGusev/gazeta', revision="v2.0")
-    df_test = pd.DataFrame(dataset['test'], columns=['text', 'summary'])
-    rouge = Rouge()
-    rouges = defaultdict(list)
-    for idx, (text, orig_summary) in enumerate(zip(df_test['text'][:n_rows], df_test['summary'][:n_rows])):
-        summarize = LsaSummarizer.summarize_text_average(text) if average else LsaSummarizer.summarize_text(text)
-        scores = rouge.get_scores(summarize, orig_summary)[0]
-        for metric, value in scores.items():
-            rouges[metric].append(value['f'])
-    table = PrettyTable()
-    table.field_names = ['', 'Lsa']
-    for metric, values in rouges.items():
-        avg_score = sum(values) / len(values)
-        table.add_row([metric, avg_score])
-    return table
+        print(f'{metric}: {sum(values) / len(values)}')
